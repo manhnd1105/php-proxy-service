52858d6fd5ef2dbb111a9ceccd842236f4af3f96
unknown <Administrator@VWLZZ3ASS0OBWO5.fpoly.edu.vn>
2013-07-12 16:16:54 +0700

    success

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$client = new SoapClient("http://ideone.com/api/1/service.wsdl");
+$sourceCode = $_POST['source'];
+$language = $_POST['lang'];
+$input = null;
+$createPaste = $client->createSubmission('manhnd','123456',$sourceCode,$language,$input,true,true);
+sleep(1);
+$pasteLink = $createPaste['link'];
+$detail = $client->getSubmissionDetails('manhnd','123456',$pasteLink,true,true,true,true,true);
+echo $detail['output'];
+return "OK";
+?>
